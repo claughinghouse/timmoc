c104b82c4a6f83979fcca554699c0e6bf90858f7
claughinghouse <claughinghouse@users.noreply.github.com>
2023-06-28 19:55:50 -0400

    Fucking egotistical bastard. adds expandtab to vimrc

--- stacks/timmocApi.ts
+++ stacks/timmocApi.ts
@@ -2,16 +2,25 @@ import { Api, StackContext } from "sst/constructs";
 
 export function API({ stack }: StackContext) {
   const api = new Api(stack, "api", {
-    // defaults: {
-    //   function: {
-    //     architecture: "arm_64",
-    //     runtime: "nodejs18.x",
-    //   },
-    // },
+    accessLog: {
+      retention: "one_year",
+    },
+    defaults: {
+      function: {
+        architecture: "arm_64",
+        runtime: "nodejs18.x",
+      },
+      throttle: {
+        rate: 10,
+      },
+    },
     routes: {
-      "GET /": "packages/functions/src/lambda.handler",
       "GET /json": "packages/functions/src/json.handler",
       "GET /text": "packages/functions/src/text.handler",
+      "GET /{id}": "",
+      "GET /json/{id}": "",
+      "GET /text/{id}": "",
+      $default: "packages/functions/src/lambda.handler",
     },
   });
 
