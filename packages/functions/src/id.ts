6e9f5d01155cd3eaf2d99edac439f9479bb92c83
claughinghouse <claughinghouse@users.noreply.github.com>
2023-07-01 15:33:48 -0400

    i hid an easter egg in the code. can you find it?

--- packages/functions/src/id.ts
+++ packages/functions/src/id.ts
@@ -3,10 +3,6 @@ import { getQuote } from "./quotePicker.js";
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
   const id = event.pathParameters?.id;
-  if (!id) {
-    return { statusCode: 404, body: "Missing ID" };
-  }
-
   const quote = getQuote(String(id));
   const value = Object.values(quote)[0];
   return { statusCode: 200, body: value };
