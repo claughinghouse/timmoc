c81b14a407c67970cc3911da0b82414785be6e17
claughinghouse <claughinghouse@users.noreply.github.com>
2023-07-01 11:15:00 -0400

    I really should've committed this when I finished it...

--- packages/functions/src/default.tsx
+++ packages/functions/src/default.tsx
@@ -2,9 +2,11 @@ import { getRandomQuote } from "@timmoc/functions/quotePicker.js";
 import { ApiHandler } from "sst/node/api";
 
 export const handler = ApiHandler(async () => {
-  const response = getRandomQuote();
+  const randomQuote = getRandomQuote();
+  // Remove the quotes from the json object
+  const response = JSON.stringify(randomQuote).replace(/^"|"$/g, "");
   return {
     statusCode: 200,
-    body: JSON.stringify(response).replace(/^"|"$/g, ""),
+    body: response,
   };
 });
