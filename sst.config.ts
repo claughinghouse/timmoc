61fe69c68017d89bce591c71be59af7eab20056e
claughinghouse <claughinghouse@users.noreply.github.com>
2024-04-10 14:34:32 -0400

    cleanup
    closes #51
    closes #52
    closes #53
    closes #54
    closes #55
    closes #56
    closes #58

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,7 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
+import { weakImport } from "sst/constructs";
+
 export default $config({
   app(input) {
     return {
@@ -9,12 +11,19 @@ export default $config({
     };
   },
   async run() {
-    const worker = new sst.cloudflare.Worker("Timmoc", {
+    const domain =
+      {
+        production: "timmoc.dev",
+        dev: "timmoc.laughinghou.se",
+      }[$app.stage] || $app.stage + "timmoc.laughinghou.se";
+
+    const timmocWorker = new sst.cloudflare.Worker("TimmocWorker", {
       handler: "packages/functions/src/index.ts",
       url: true,
+      domain: domain,
     });
     return {
-      url: worker.url,
+      url: timmocWorker.url,
     };
   },
 });
