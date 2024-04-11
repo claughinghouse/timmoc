0ab5a8ee1b24cce409b699c41e3b4e997d81d577
claughinghouse <claughinghouse@users.noreply.github.com>
2024-04-10 21:49:56 -0400

    cf deployment functional

--- sst.config.ts
+++ sst.config.ts
@@ -13,12 +13,15 @@ export default $config({
       {
         production: "timmoc.dev",
         dev: "dev.timmoc.dev",
-      }[$app.stage] || $app.stage + "dev.timmoc.dev";
+      }[$app.stage] || $app.stage + ".dev.timmoc.dev";
+
+    const timmocDb = new sst.cloudflare.D1("TimmocDb");
 
     const timmocWorker = new sst.cloudflare.Worker("TimmocWorker", {
       handler: "packages/functions/src/index.ts",
       url: true,
       domain: domain,
+      link: [timmocDb],
     });
     return {
       url: timmocWorker.url,
