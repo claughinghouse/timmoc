f4c146333454a25c72c149b746591e2ab7d9c3bb
claughinghouse <claughinghouse@users.noreply.github.com>
2024-04-20 11:27:56 -0400

    Add GH workflow

--- packages/core/src/quotes/seed.sql
+++ packages/core/src/quotes/seed.sql
@@ -4,13 +4,13 @@ CREATE TABLE d1_migrations(
 		name       TEXT UNIQUE,
 		applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
-INSERT INTO d1_migrations VALUES(1,'0000_classy_swarm.sql','2024-04-16 22:48:01');
+INSERT INTO d1_migrations VALUES(1,'0000_classy_swarm.sql','2024-04-17 16:49:54');
 CREATE TABLE `quotes` (
 	`id` text NOT NULL,
 	`quote_text` text NOT NULL
 );
-INSERT INTO quotes VALUES('1','this is the first quote');
-INSERT INTO quotes VALUES('2','this is the second quote');
-INSERT INTO quotes VALUES('3','this is the third quote');
+INSERT INTO quotes VALUES('clv41o1f1000008l01103e288','this is the first quote');
+INSERT INTO quotes VALUES('clv41oim7000108l0d7kedu2q','this is the second quote ');
+INSERT INTO quotes VALUES('clv41op1v000208l08r2lfpqw','this is the third quote');
 DELETE FROM sqlite_sequence;
 INSERT INTO sqlite_sequence VALUES('d1_migrations',1);
--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -7,6 +7,7 @@ app.get("/", async (c) => {
   const quote = await Resource.TimmocDb.prepare(
     "SELECT * FROM quotes ORDER BY RANDOM() LIMIT 1",
   ).run();
+
   return c.text(quote.results[0].quote_text);
 });
 
