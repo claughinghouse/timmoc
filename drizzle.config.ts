28c0da9d724a09ddca0d74402c48c57f6ccaf47d
claughinghouse <claughinghouse@users.noreply.github.com>
2025-11-25 22:10:54 -0500

    Add Sandbox

---
+++ drizzle.config.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  out: "./drizzle",
+  schema: "./src/db/schema.ts",
+  dialect: "sqlite",
+  driver: "durable-sqlite",
+});
---
+++ drizzle/migrations.js
@@ -0,0 +1,10 @@
+import journal from './meta/_journal.json';
+import m0000 from './0000_vengeful_charles_xavier.sql';
+
+  export default {
+    journal,
+    migrations: {
+      m0000
+    }
+  }
+  
