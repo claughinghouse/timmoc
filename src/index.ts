5f2e8d77d3c3f26cd5fda6f0132d0ef011379b8b
claughinghouse <claughinghouse@users.noreply.github.com>
2025-11-28 11:24:25 -0500

    I CAN HAZ PYTHON, I CAN HAZ INDENTS

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import quotes from "./quotes";
 
 export default {
-  async fetch(_request: Request, _env: Env): Promise<Response> {
-    const quote = quotes[Math.floor(Math.random() * quotes.length)];
+  async fetch(request: Request, _env: Env): Promise<Response> {
+    const url = new URL(request.url);
 
-    return new Response(quote.toString());
+    if (url.pathname === "/") {
+      const quote = quotes[Math.floor(Math.random() * quotes.length)];
+
+      return new Response(quote.toString());
+    }
+
+    return new Response("Not found", { status: 404 });
   },
 };
